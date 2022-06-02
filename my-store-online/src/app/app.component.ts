97e9a5d7c4391871fd86bf042c8b3df0768f31bd
Joan Luna <alexis_dx8@hotmail.com>
2022-06-01 19:49:13 -0700

    creando componente product y mandandolo desde componente padre para ser renderizado

--- my-store-online/src/app/app.component.ts
+++ my-store-online/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Product} from'./models/product.models';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+  products: Product[] = [{
+    id: '1',
+    name: 'EL mejor juguete',
+    price: 565,
+    image: './assets/img/agucate.png'
+  },
+  {
+    id: '2',
+    name: 'Bicicleta casi nueva',
+    price: 356,
+    image: './assets/img/perrito3.jpg'
+  },
+  {
+    id: '3',
+    name: 'Colleción de albumnes',
+    price: 34,
+    image: './assets/img/perrito2.webp'
+  },
+  {
+    id: '4',
+    name: 'Mis libros',
+    price: 23,
+    image: './assets/img/perrito1.jpg'
+  },];
 
   onLoaded(img: string){
     console.log('log padre', img);
