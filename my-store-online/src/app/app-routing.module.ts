99e07ab69ac5aef78dcddeb6f48bcfdfafaf5f93
joan sistemas <alexis_dx8@hotmail.com>
2022-06-09 10:17:21 -0700

    product/detail

--- my-store-online/src/app/app-routing.module.ts
+++ my-store-online/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+
 
 const routes: Routes = [
   {
@@ -24,6 +26,11 @@ const routes: Routes = [
     path:'category/:id',
     component: CategoryComponent
   },
+  {
+    path:'product/:id',
+    component: ProductDetailComponent
+  }
+  ,
   {
   path:'login',
   component: LoginComponent
---
+++ my-store-online/src/app/pages/product-detail/product-detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+
+import { Product } from '../../models/product.models';
+import { ProductsService } from '../../services/products.service';
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+  
+  productID: string | null=null;
+  product: Product | null=null;
+
+
+  constructor(
+    private route:ActivatedRoute,
+    private productservice: ProductsService
+    ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.
+    pipe(
+      switchMap(params=>{
+        this.productID = params.get('id');
+      if(this.productID){
+       return this.productservice.getProduct(this.productID)
+      }
+      return[null];
+      })
+    ).
+    subscribe((data)=>{
+      this.product=data
+    }
+     
+    )
+  }
+
+}
