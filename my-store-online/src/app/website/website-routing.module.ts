3635191e006f1215078b3d22d4cd8e9fbdd58dec
joan sistemas <alexis_dx8@hotmail.com>
2022-06-10 10:52:09 -0700

    preload standar y custom

--- my-store-online/src/app/website/website-routing.module.ts
+++ my-store-online/src/app/website/website-routing.module.ts
@@ -23,7 +23,10 @@ const routes: Routes = [{
     },
     {
       path:':category',
-      loadChildren:()=>import('./pages/category/category.module').then(m=>m.CategoryModule)
+      loadChildren:()=>import('./pages/category/category.module').then(m=>m.CategoryModule),
+      data:{
+        preload:true,
+      }
 
     },
     {
