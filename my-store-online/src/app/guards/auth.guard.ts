0000501e64b10a6039ce7fd0f911d5d6b32c64b9
Joan Luna <alexis_dx8@hotmail.com>
2022-06-12 13:32:23 -0700

    todo correcto

--- my-store-online/src/app/guards/auth.guard.ts
+++ my-store-online/src/app/guards/auth.guard.ts
@@ -3,29 +3,18 @@ import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } fro
 import { Observable } from 'rxjs';
 import { TokenService } from '../services/token.service';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-<<<<<<< HEAD
   constructor(private token:TokenService){}
-=======
-  constructor(
-    private tokenservice:TokenService
-  ){
-
-  }
->>>>>>> origin/master
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-<<<<<<< HEAD
       const token = this.token.getToken();
-=======
-    const token = this.tokenservice.getToken();
->>>>>>> origin/master
     return token? true:false;
   }
   
